82a3e8f3ba455765bff994d5fe757d6c1bdc87fe
heartandkarma <heartandkarma@gmail.com>
2022-01-13 20:09:20 -0800

    got 8 working

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ function removeArtist(array, number) {
   curArray.splice(number, 1);
   return curArray.length;
 }
-console.log(removeArtist(artists, 18));
+// console.log(removeArtist(artists, 18));
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
 1. Receive an array
@@ -334,18 +334,28 @@ function lotsOfArt(array) {
   }
   return returnArr;
 }
-console.log(lotsOfArt(artists));
+// console.log(lotsOfArt(artists));
+
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 8: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Use artistInfo to do the following: 
+Use artistInfo to do the following:  
 1. Receive an array 
 2. Receive an artist's name
 3. return the artists bio
-
+ 
 For example artistInfo(artists, 'Frida Kahlo') will return: 
   ["Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."]
 */
-function artistInfo(/*Your Code Here*/) {
-  /*Your Code Here*/
+
+function artistInfo(array, artist) {
+  const result = [];
+  for (var i = 0; i < array.length; i++) {
+    const searchFor = array[i].name;
+    const bio = array[i].bio;
+    if (searchFor === artist) {
+      result.push(bio);
+    }
+  }
+  return result;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
